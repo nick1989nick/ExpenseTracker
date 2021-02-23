9eea901dd18d06dbb60aa95c4004d2271ea65c4d
Nikola Jurkovic <jurkovicnikola89@gmail.com>
2021-02-23 09:44:21 +0100

    start on app >>>>>>> Stashed changes

---
+++ ExpenseTracker/UI/Balance/BalanceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Nikola Jurkovic on 23/02/2021.
+//
+
+import UIKit
+
+class BalanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExpenseTracker/UI/Base/BaseViewController.swift
@@ -0,0 +1,10 @@
+//
+//  BaseViewController.swift
+//  ExpenseTracker
+//
+//  Created by Nikola Jurkovic on 23/02/2021.
+//
+
+import Foundation
+import UIKit
+
